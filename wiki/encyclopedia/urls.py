da21a94e22413ce48ae78dee4a68913c37ebf5a6
Mr. Baker <ojibweeagle@outlook.com>
2025-10-15 20:17:12 -0500

    Take out notes

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-    path("wiki/str:entry>" views.entry, name="entry"),
-    path("newEntry", views.newEntry, names="newEntry"),
-    path("wiki/str:entry>/edit", views.edit, name="edit"),
-    path("random", views.random, name="random"),
-    path("search", views.search, name="search")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("create", views.create, name="create"),
+    path("edit/<str:title>", views.edit, name="edit"),
+    path("random_title", views.random_title, name="random_title"),
 ]
